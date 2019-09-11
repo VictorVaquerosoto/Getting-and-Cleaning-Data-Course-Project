9424d8b53690ea9b8358a127baecd83307e1207f
VictorVaquerosoto <54574705+VictorVaquerosoto@users.noreply.github.com>
2019-09-11 10:56:04 +0200

    Last one

--- run_analysis.R
+++ run_analysis.R
@@ -4,6 +4,7 @@ library(dplyr)
 
 ##Check if the directory exists, if nor we create it:
 if(!file.exists("./data")){dir.create("./data")}
+
 url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
 ##Check if we have already downloaded the zip file, if not we do it:
